f9472b443a8e8c196b469e30c65897bd6394399a
= <kanva001@gmail.com>
2024-05-17 23:14:19 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    
+    name ="src",
+    version = "0.0.1",
+    description = "This is winequality MLOPS package",
+    author = "Vasanth",
+    packages = find_packages(),
+    license = "MIT"
+    
+    
+)
